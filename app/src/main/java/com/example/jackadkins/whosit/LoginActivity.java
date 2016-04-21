4a565d0008b5cbfa3a33ccbf0a0543352f057918
zumwaldez1 <zach0861@gmail.com>
2016-04-21 19:02:29 -0400

    Updated LoginActivity
    The Register Button will now open the RegisterActivity

--- app/src/main/java/com/example/jackadkins/whosit/LoginActivity.java
+++ app/src/main/java/com/example/jackadkins/whosit/LoginActivity.java
@@ -1,13 +1,50 @@
 package com.example.jackadkins.whosit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements OnClickListener {
+
+    EditText usernameEditText;
+    EditText passwordEditText;
+    Button loginButton;
+    Button registerButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        registerButton = (Button) findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        //Class cActivity = null;
+        String launcher = "com.example.jackadkins.whosit.RegisterActivity";
+        Intent i = null;
+        try
+        {
+            i = new Intent(getApplicationContext(), Class.forName(launcher));
+        }
+        catch (ClassNotFoundException e)
+        {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            return;
+        }
+        startActivity(i);
     }
 }
