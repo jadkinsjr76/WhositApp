bbbdf2efe7ffda03b4da84ae63171560419778e0
Nathaniel <nchudson88@gmail.com>
2016-04-22 21:11:36 -0400

    Profile Revision
    Attempt to make Profile display quizzes. Currently not working, but, I
    thought I would offer it for now.

--- app/src/main/java/com/example/jackadkins/whosit/ProfileActivity.java
+++ app/src/main/java/com/example/jackadkins/whosit/ProfileActivity.java
@@ -1,13 +1,104 @@
 package com.example.jackadkins.whosit;
 
+import android.content.Context;
+import android.drm.DrmStore;
+//import android.drm.DrmStore.Action;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
 
 public class ProfileActivity extends AppCompatActivity {
+
+
+    // Define variables for the widgets:
+    private ListView profileQuizzesListView;
+    private int userID = 1;
+    private WhosItDB db = new WhosItDB(this.getApplicationContext());
+
+    // Define additional variables for the class:
+    private ArrayAdapter<String> listAdapter;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        // Get references to the widgets:
+        profileQuizzesListView = (ListView) findViewById(R.id.profile_quizzes_listView);
+
+
+        /**
+         * The code below is example code on how to add to a ListView... It is NOT permanent code.
+         */
+
+        // Create and populate a List of planet names.
+        ArrayList<String> quizList = new ArrayList<String>();
+        ArrayList<Quiz> quizzes = db.getQuizzes(userID);
+
+        for (int i = 0; i < quizzes.size(); i++) {
+            quizList.add(quizzes.get(i).getName());
+        }
+
+        // Create ArrayAdapter using the planet list.
+        listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, quizList);
+
+        // Set the ArrayAdapter as the ListView's adapter.
+        profileQuizzesListView.setAdapter(listAdapter);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Profile Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.jackadkins.whosit/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Profile Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.jackadkins.whosit/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
