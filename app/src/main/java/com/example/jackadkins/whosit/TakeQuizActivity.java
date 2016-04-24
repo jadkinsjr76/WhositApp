3ed8d3d9e7a6726bb76b781147f6b961fb12099a
zumwaldez1 <zach0861@gmail.com>
2016-04-24 19:47:53 -0400

    Revert "Normal Commit"
    This reverts commit 56d68b32bde0637449df1bc9df8286c5ef6affab.

--- app/src/main/java/com/example/jackadkins/whosit/TakeQuizActivity.java
+++ app/src/main/java/com/example/jackadkins/whosit/TakeQuizActivity.java
@@ -9,22 +9,7 @@ import java.io.FileNotFoundException;
 
 public class TakeQuizActivity extends AppCompatActivity
 {
-<<<<<<< Updated upstream
     Quiz quiz = null;
-=======
-    // Widget class variables:
-    private TextView    questionTextView;
-    private RadioButton answer1;
-    private RadioButton answer2;
-    private RadioButton answer3;
-    private RadioButton answer4;
-
-    // Class instance variables:
-    private WhosItDB          db;
-    private Quiz              quiz;
-    private Question          questions;
-    private int               quizID;
->>>>>>> Stashed changes
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -32,19 +17,9 @@ public class TakeQuizActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_quiz);
 
-<<<<<<< Updated upstream
         getSupportActionBar().hide();
-=======
-        // Initialize variables to widgets:
-        questionTextView = (TextView)    findViewById(R.id.quiz_question_textView);
-        answer1          = (RadioButton) findViewById(R.id.answer_radio1);
-        answer2          = (RadioButton) findViewById(R.id.answer_radio2);
-        answer3          = (RadioButton) findViewById(R.id.answer_radio3);
-        answer4          = (RadioButton) findViewById(R.id.answer_radio4);
->>>>>>> Stashed changes
 
         Intent intent = getIntent();
-<<<<<<< Updated upstream
         int quizId = intent.getIntExtra("quizId", -1);
 
         if(quizId >= 0)
@@ -52,29 +27,6 @@ public class TakeQuizActivity extends AppCompatActivity
             // go to database and get quiz info and build the quiz object
             getQuizById(quizId);
         }
-=======
-        quizID = intent.getIntExtra("QUIZ_ID", -1);
-
-        // Get quiz from the database, load in the questions for the quiz, load all answers:
-        db = new WhosItDB(this);
-        quiz = db.getQuiz(quizID);
-        quiz.setAllQuestions(db.getQuestions(quizID));
-        for(int i = 0; i < quiz.getNumQuestions(); i++) {
-            quiz.getQuestion(i).setAllAnswers(db.getAnswers(quiz.getQuestion(i).getQuestionID()));
-        }
-
-        int questionNum = 0;
-
-        // Display information:
-        Question questionObject = quiz.getQuestion(questionNum);
-        questionTextView.setText(questionObject.getQuestionText());
-
-        answer1.setText(quiz.getQuestion(questionNum).getAnswer(0).getAnswerName());
-        answer2.setText(quiz.getQuestion(questionNum).getAnswer(1).getAnswerName());
-        answer3.setText(quiz.getQuestion(questionNum).getAnswer(2).getAnswerName());
-        answer4.setText(quiz.getQuestion(questionNum).getAnswer(3).getAnswerName());
-
->>>>>>> Stashed changes
     }
 
     private void getQuizById(int quizId)
