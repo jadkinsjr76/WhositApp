f99b863706adcaa357a855d8665cd1b8cabfcf49
Nathaniel <nchudson88@gmail.com>
2016-04-24 20:09:05 -0400

    Commit To End All Commits

--- app/src/main/java/com/example/jackadkins/whosit/ProfileActivity.java
+++ app/src/main/java/com/example/jackadkins/whosit/ProfileActivity.java
@@ -9,12 +9,18 @@ import android.drm.DrmStore;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -22,7 +28,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
 
-public class ProfileActivity extends AppCompatActivity implements OnClickListener {
+public class ProfileActivity extends AppCompatActivity {
 
 
     // Define variables for the widgets:
@@ -34,14 +40,15 @@ public class ProfileActivity extends AppCompatActivity implements OnClickListene
 
     // Define additional variables for the class:
     private ArrayAdapter<String> listAdapter;
-    private Context context;
+    private ArrayList<String>  quizList;
+    private ArrayList<Integer> quizIDs;
+    private ArrayList<Quiz>    quizzes;
 
-
-    ArrayList<String>  quizList;
-    ArrayList<Integer> quizIDs;
-    ArrayList<Integer> viewIDs;
-    ArrayList<Quiz>    quizzes;
-
+    /**
+     * This initializes and creates the instance of the profile activity.
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,11 +56,14 @@ public class ProfileActivity extends AppCompatActivity implements OnClickListene
         // Get the intent which has started the activity:
         Intent intent = getIntent();
 
-        // Initialize the user's values:
+        // Initialize the user's values using intents:
         userID = intent.getIntExtra("userId", 0);
         usernameString = intent.getStringExtra("username");
+
+        // Start up the database:
         db = new WhosItDB(this);
 
+        // Set the content view to ProfileActivity:
         setContentView(R.layout.activity_profile);
 
         // Get references to the widgets:
@@ -63,28 +73,35 @@ public class ProfileActivity extends AppCompatActivity implements OnClickListene
         // Display the username to the user:
         usernameTextView.setText(usernameString);
 
-        /**
-         * The code below is example code on how to add to a ListView... It is NOT permanent code.
-         */
-
         // Create and populate a List of planet names.
-        ArrayList<String>  quizList = new ArrayList<String>();
-        ArrayList<Integer> quizIDs = new ArrayList<Integer>();
-        ArrayList<Integer> viewIDs = new ArrayList<Integer>();
-        ArrayList<Quiz>    quizzes = db.getQuizzes(userID);
+        quizList = new ArrayList<String>();
+        quizIDs  = new ArrayList<Integer>();
+        quizzes  = db.getQuizzes(userID);
 
-
+        // Iterate through 'quizzes' and add each to the profile:
         for (int i = 0; i < quizzes.size(); i++) {
-            quizList.add("1 - " + quizzes.get(i).getName());
+            quizList.add(quizzes.get(i).getName());
             quizIDs.add(quizzes.get(i).getQuizID());
         }
 
         // Create ArrayAdapter using the planet list.
         listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, quizList);
 
-        // Set the ArrayAdapter as the ListView's adapter.
+        // Set the ArrayAdapter as the ListView's adapter, and add the respective listeners:
         profileQuizzesListView.setAdapter(listAdapter);
+        profileQuizzesListView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Put extra data and then start activity:
+                Intent intent = new Intent(getApplicationContext(), TakeQuizActivity.class);
+                intent.putExtra("QUIZ_ID", quizIDs.get(position));
 
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {}
+
+                startActivity(intent);
+            }
+        });
     }
 
     // TODO: Fill in.
@@ -99,20 +116,42 @@ public class ProfileActivity extends AppCompatActivity implements OnClickListene
 //        super.onStop();
 //    }
 
+    /**
+     * This overridden method will handle and display the menu for the Profile Activity.
+     *
+     * @param menu
+     * @return
+     */
     @Override
-    public void onClick(View v) {
-        // Declare and initialize variables:
-        TextView text = (TextView) v;
-        String viewText = text.getText().toString();
-        int index = Integer.parseInt(viewText.split(" ")[0]);
-        int quizID = quizIDs.get(index);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_profile, menu);
+        return true;
+    }
 
-        // Debug:
-        System.out.println("DEBUG - quizID: " + quizID);
+    /**
+     * This method will handle when something in the menu is clicked.
+     *
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Variable initialization:
+        Intent intent;
 
-        // Start the new activity:
-        Intent intent = new Intent(this, TakeQuizActivity.class);
-        intent.putExtra("QUIZ_ID", quizID);
-        startActivity(intent);
+        // Switch statement to handle cases:
+        switch (item.getItemId()) {
+            case R.id.action_addQuiz:
+                intent = new Intent(getApplicationContext(), CreateQuizActivity.class);
+                intent.putExtra("USER_ID", userID);
+                startActivity(intent);
+                break;
+
+            case R.id.action_logout:
+                intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/jackadkins/whosit/TakeQuizActivity.java
+++ app/src/main/java/com/example/jackadkins/whosit/TakeQuizActivity.java
@@ -3,13 +3,30 @@ package com.example.jackadkins.whosit;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class TakeQuizActivity extends AppCompatActivity
 {
-    Quiz quiz = null;
+
+    // Widget class variables:
+    private TextView    questionTextView;
+    private RadioButton answer1;
+    private RadioButton answer2;
+    private RadioButton answer3;
+    private RadioButton answer4;
+
+    // Class instance variables:
+    private WhosItDB          db;
+    private Quiz              quiz;
+    private Question          questions;
+    private Question          tempQuestion;
+    private int               quizID;
+    private ArrayList<Answer> answers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,7 +34,12 @@ public class TakeQuizActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_quiz);
 
-        getSupportActionBar().hide();
+        // Initialize variables to widgets:
+        questionTextView = (TextView)    findViewById(R.id.quiz_question_textView);
+        answer1          = (RadioButton) findViewById(R.id.answer_radio1);
+        answer2          = (RadioButton) findViewById(R.id.answer_radio2);
+        answer3          = (RadioButton) findViewById(R.id.answer_radio3);
+        answer4          = (RadioButton) findViewById(R.id.answer_radio4);
 
         Intent intent = getIntent();
         int quizId = intent.getIntExtra("quizId", -1);
@@ -27,6 +49,30 @@ public class TakeQuizActivity extends AppCompatActivity
             // go to database and get quiz info and build the quiz object
             getQuizById(quizId);
         }
+        quizID = intent.getIntExtra("QUIZ_ID", -1);
+
+        // Get quiz from the database, load in the questions for the quiz, load all answers:
+        db = new WhosItDB(this);
+        quiz = db.getQuiz(quizID);
+        quiz.setAllQuestions(db.getQuestions(quizID));
+
+        for (int i = 0; i < quiz.getNumQuestions(); i++) {
+            tempQuestion = quiz.getQuestion(i);
+            answers = db.getAnswers(tempQuestion.getQuestionID());
+            tempQuestion.setAllAnswers(answers);
+        }
+
+        int questionNum = 0;
+
+        // Display information:
+        Question question = quiz.getQuestion(questionNum);
+        questionTextView.setText(question.getQuestionText());
+
+//        answer1.setText(question.getAnswer(0).getAnswerName());
+//        answer2.setText(question.getAnswer(1).getAnswerName());
+//        answer3.setText(question.getAnswer(2).getAnswerName());
+//        answer4.setText(question.getAnswer(3).getAnswerName());
+
     }
 
     private void getQuizById(int quizId)
